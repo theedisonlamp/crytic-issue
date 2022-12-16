d66e6d577d66868682cf6c6e0e9a9e44d18c23fa
The Edison Lamp <daniel@usual.company>
2022-12-16 20:38:21 +0000

    :construction: Apply workaround from https://github.com/crytic/slither/issues/1509\#issuecomment-1355378418.

--- solidity/src/Counter.sol
+++ solidity/src/Counter.sol
@@ -1,28 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.16;
 
-contract
-    Counter
-{
-    uint256
-        public
-        number;
+contract Counter {
+    uint256 public number;
 
-    function setNumber(
-        uint256
-            newNumber
-    )
-        public
-    {
-        number
-        =
-            newNumber;
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
     }
 
-    function increment(
-    )
-        public
-    {
+    function increment() public {
         number++;
     }
 }
